60d1d3dfff42b42ea7e932b9f577a04f16d3789e
aleksey <lomantsov92@mail.ru>
2022-06-29 02:48:04 +0300

    add task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 5. Напишите программу, которая на вход принимает
+// одно число(N), а на выходе показывает все елые числа в промежутке от -N до N.
+
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int index = -num; index <= num; index ++)
+{
+    Console.Write($"{index} ");
+}
