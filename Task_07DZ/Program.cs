0f5d4470709a8566865a58c41e3d0e7f5c89d256
aleksey <lomantsov92@mail.ru>
2022-06-29 23:52:26 +0300

    Add task_07DZ

---
+++ Task_07DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 7: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Программа выдает посленюю из трех цифр");
+Console.WriteLine("Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (99 < num & num < 1000 )
+{ 
+    Console.WriteLine("Верно");
+    int lastNumber = num % 10;
+    Console.Write("Последняя цифра: ");
+    Console.Write(lastNumber);
+}
+else
+{
+    Console.WriteLine("Введино не трехзначное число");
+}
