6e22d8d19cd1083e27ea4d6c735cc246e167cf0b
aleksey <lomantsov92@mail.ru>
2022-07-07 23:01:58 +0300

    add ask 26 and 28

---
+++ Task_26/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("Введите число: "); 
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int DigitalC(int a)
+// {
+                                                        //не верно
+//     int digital = 0;
+//     for (int i = 0; i <= a; i++)
+//     {
+//         a = a / 10;
+//         digital++;
+//     }
+//     return digital;
+// }
+// int result = DigitalC(num);
+// Console.Write($"Колличество цифр в числе {num} = {result}");
+
+Console.WriteLine("Введите число для подсчета кол-ва чисел в нем: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int digitsCount(int a)
+{
+int digitsCounter = 0;
+while (a != 0)
+{
+a /= 10;
+digitsCounter++;
+}
+return digitsCounter;
+}
+int amountOfDigits = (digitsCount(num));
+Console.Write($"В числе {num} {amountOfDigits} цифры");
---
+++ Task_28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Digital(int number)
+{
+    int temp= 1;
+    for (int i = 1; i <= number; i++)
+    {
+    temp = i * temp;
+    }
+    return temp;
+}
+int result = Digital(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} = {result}");
