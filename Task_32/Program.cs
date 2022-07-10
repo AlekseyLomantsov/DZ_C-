35dcdbce666ae26f4fdfea132c9c40b328b19091
aleksey <lomantsov92@mail.ru>
2022-07-10 22:34:05 +0300

    add task 32and 33

---
+++ Task_32/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreteArreyRndInt(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+int[] GetSumNegPosElem(int[] array)
+{
+    int[] sumNeg = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+            sumNeg[i] = array[i] * -1;  
+    }
+    return sumNeg;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+Console.Write("Введите размер массива: ");
+int insize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] resArray = CreteArreyRndInt(insize, min, max);
+PrintArray(resArray);
+int[] SumNegPosElem = GetSumNegPosElem(resArray);
+Console.Write(" -> ");
+PrintArray(SumNegPosElem);
---
+++ Task_33/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreteArreyRndInt(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+bool SearchElem(int[] array, int elem) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == elem) return true;
+    }
+    return false;
+}
+
+void PrintRes(bool result, int element)
+{
+    string res = result ? $"Да, цифра {element} есть в этом массиве": $"Нет, цифры {element} нет в этом массиве";
+    Console.WriteLine(res);
+}
+
+int[] result = CreteArreyRndInt(5, -6, 9);
+PrintArray(result);
+bool resSearchElem = SearchElem(result, 4);
+PrintRes(resSearchElem, 4);
+
+
