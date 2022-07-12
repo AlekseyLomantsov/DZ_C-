f9b04e639948b3858fe177cb95a7bc72c8534845
aleksey <lomantsov92@mail.ru>
2022-07-12 22:36:23 +0300

    add task 44and 45

---
+++ Task_44/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first =0;
+int second=1;
+int fibonachi=0;
+
+Console.Write($"{first} ");
+Console.Write($"{second} ");
+
+for (int i = 2; i < number; i++)
+{
+    fibonachi= first+second;
+    Console.Write($"{fibonachi} ");
+    first=second;
+    second=fibonachi;
+}
+
---
+++ Task_45/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] CreteArreyRndInt(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] CopyArray(int[] array)
+{
+int[] copy = new int[array.Length];
+for(int i = 0; i<copy.Length; i++)
+{
+    copy[i]= array[i];
+}
+return copy;
+}
+
+int[] mass= CreteArreyRndInt(6,1,10);
+PrintArray(mass);
+int[] res =CopyArray(mass);
+PrintArray(res);
