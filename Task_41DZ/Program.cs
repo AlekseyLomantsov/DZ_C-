71d7d11384ee7df152b82deec5265ac303982b4e
aleksey <lomantsov92@mail.ru>
2022-07-13 14:10:05 +0300

    add task 41and 43DZ

---
+++ Task_41DZ/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите колличество цифр: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Вводите цифры по 1: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+void ArrMoreZero(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    Console.WriteLine($"Чисел больше нуля введено {count}");
+}
+int[] array = Array(size);/*{0, 7, 8, -2, -2};*/
+ArrMoreZero(array);
---
+++ Task_43DZ/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите данные для уровнений: y = k1 * x + b1, y = k2 * x + b2");
+
+Console.Write("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double Equations1(double b1, double k1, double b2, double k2)
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    return x;
+}
+
+double Equations2(double x, double k2, double b2)
+{
+    double y = k2 * x + b2;
+    return y;
+}
+
+void PrintXandY(double x, double y)
+{
+    Console.WriteLine($"Точка пересечения: ({x}; {y})");
+}
+
+double x = Equations1(b1, k1, b2, k2);
+double y = Equations2(x, k2, b2);
+PrintXandY(x, y);
+
+
